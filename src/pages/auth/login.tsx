1800d0e1e21b93c4bf5526fbe7c9932d32215a1d
rca <nem@nem.x0.to>
2025-01-08 12:29:58 +0900

    remove unused tag

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -27,49 +27,47 @@ export const Login = () => {
 
   return (
     <AuthLayout>
-      <>
-        <div className="text-center text-[25px] font-bold mb-6">ログイン</div>
+      <div className="text-center text-[25px] font-bold mb-6">ログイン</div>
 
-        <Formik
-          initialValues={{ username: "", password: "" }}
-          validationSchema={toFormikValidationSchema(loginFormSchema)}
-          onSubmit={handleLogin}
-        >
-          {({ values, errors, touched, handleChange, handleSubmit }) => (
-            <>
-              <div className="flex flex-col w-1/2 gap-4 mb-4">
-                <Input
-                  variant="bordered"
-                  label="ユーザー名"
-                  type="text"
-                  value={values.username}
-                  isInvalid={!!errors.username && !!touched.username}
-                  errorMessage={errors.username}
-                  onChange={handleChange("username")}
-                />
-                <Input
-                  variant="bordered"
-                  label="パスワード"
-                  type="password"
-                  value={values.password}
-                  isInvalid={!!errors.password && !!touched.password}
-                  errorMessage={errors.password}
-                  onChange={handleChange("password")}
-                />
-              </div>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validationSchema={toFormikValidationSchema(loginFormSchema)}
+        onSubmit={handleLogin}
+      >
+        {({ values, errors, touched, handleChange, handleSubmit }) => (
+          <>
+            <div className="flex flex-col w-1/2 gap-4 mb-4">
+              <Input
+                variant="bordered"
+                label="ユーザー名"
+                type="text"
+                value={values.username}
+                isInvalid={!!errors.username && !!touched.username}
+                errorMessage={errors.username}
+                onChange={handleChange("username")}
+              />
+              <Input
+                variant="bordered"
+                label="パスワード"
+                type="password"
+                value={values.password}
+                isInvalid={!!errors.password && !!touched.password}
+                errorMessage={errors.password}
+                onChange={handleChange("password")}
+              />
+            </div>
 
-              <Button onPress={() => handleSubmit()} variant="flat" color="primary">
-                ログイン
-              </Button>
-            </>
-          )}
-        </Formik>
+            <Button onPress={() => handleSubmit()} variant="flat" color="primary">
+              ログイン
+            </Button>
+          </>
+        )}
+      </Formik>
 
-        <div className="font-light text-slate-400 mt-4 text-sm">
-          {/* <Link href="/register" className="font-bold">新規登録</Link> */}
-          <Link to="/register" className="font-bold"> 新規登録</Link>
-        </div>
-      </>
+      <div className="font-light text-slate-400 mt-4 text-sm">
+        {/* <Link href="/register" className="font-bold">新規登録</Link> */}
+        <Link to="/register" className="font-bold"> 新規登録</Link>
+      </div>
     </AuthLayout>
   );
 };
