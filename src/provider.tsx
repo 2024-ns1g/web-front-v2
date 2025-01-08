902b148741412bfb9191708d815f35e225b50de1
rca <nem@nem.x0.to>
2025-01-08 11:03:23 +0900

    add provider

--- src/provider.tsx
+++ src/provider.tsx
@@ -3,6 +3,7 @@ import type { NavigateOptions } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/system";
 import { useHref, useNavigate } from "react-router-dom";
 import ToastProvider from "./contexts/toast-context";
+import { ApiClientProvider } from "./contexts/api-client-context";
 
 declare module "@react-types/shared" {
   interface RouterConfig {
@@ -16,7 +17,9 @@ export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider navigate={navigate} useHref={useHref}>
       <ToastProvider>
-        {children}
+        <ApiClientProvider>
+          {children}
+        </ApiClientProvider>
       </ToastProvider>
     </NextUIProvider>
   );
