016c366a0feb72447345fed7092abbc0d5693687
rca <nem@nem.x0.to>
2025-01-08 12:19:52 +0900

    impl login page wip

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+import { Formik } from "formik";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export const Login = () => {
+  return (
+    <>
+      <div className="text-center text-[25px] font-bold mb-6">ログイン</div>
+
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validationSchema={LoginSchema}
+        onSubmit={handleLogin}
+      >
+        {({ values, errors, touched, handleChange, handleSubmit }) => (
+          <>
+            <div className="flex flex-col w-1/2 gap-4 mb-4">
+              <Input
+                variant="bordered"
+                label="ユーザー名"
+                type="text"
+                value={values.username}
+                isInvalid={!!errors.username && !!touched.username}
+                errorMessage={errors.username}
+                onChange={handleChange("username")}
+              />
+              <Input
+                variant="bordered"
+                label="パスワード"
+                type="password"
+                value={values.password}
+                isInvalid={!!errors.password && !!touched.password}
+                errorMessage={errors.password}
+                onChange={handleChange("password")}
+              />
+            </div>
+
+            <Button onPress={() => handleSubmit()} variant="flat" color="primary">
+              ログイン
+            </Button>
+          </>
+        )}
+      </Formik>
+
+      <div className="font-light text-slate-400 mt-4 text-sm">
+        {/* <Link href="/register" className="font-bold">新規登録</Link> */}
+        <Link to="/register" className="font-bold"> 新規登録</Link>
+      </div>
+    </>
+  );
+};
