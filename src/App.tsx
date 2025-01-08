5901333b995f0ed3a70585105035cd93cb17641f
rca <nem@nem.x0.to>
2025-01-08 12:48:01 +0900

    add path, test require auth

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import BlogPage from "@/pages/blog";
 import AboutPage from "@/pages/about";
 import { Login } from "./pages/auth/login";
 import { Register } from "./pages/auth/register";
+import DebugPage from "./pages/debug";
+import { RouteAuthGuard } from "./pages/router-auth-guard";
 
 function App() {
   return (
@@ -16,6 +18,11 @@ function App() {
       <Route element={<PricingPage />} path="/pricing" />
       <Route element={<BlogPage />} path="/blog" />
       <Route element={<AboutPage />} path="/about" />
+      <Route path="/debug" element={
+        <RouteAuthGuard component={<DebugPage />}
+        />
+      } />
+
 
       <Route element={<Login />} path="/auth/login" />
       <Route element={<Register />} path="/auth/register" />
