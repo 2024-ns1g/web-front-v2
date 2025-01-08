a648818a6e446e97375ea723c8dedcddb989d220
rca <nem@nem.x0.to>
2025-01-08 12:11:17 +0900

    rename

--- src/layouts/authLayout.tsx
+++ src/layouts/authLayout.tsx
@@ -4,7 +4,7 @@ interface Props {
   children: React.ReactNode;
 }
 
-export const AuthLayoutWrapper = ({ children }: Props) => {
+export const AuthLayout = ({ children }: Props) => {
   return (
     <div className="flex h-screen">
       <div className="flex-1 flex-col flex items-center justify-center p-6">
