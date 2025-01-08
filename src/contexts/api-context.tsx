a538c2541d957a7cab64e703b2b60d7c69b905a4
rca <nem@nem.x0.to>
2025-01-08 12:01:27 +0900

    add api context

---
+++ src/contexts/api-context.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, ReactNode } from 'react';
+import { apiEndpoints } from '@/apis/endpoints';
+
+interface ApiContextValue {
+  apis: typeof apiEndpoints;
+}
+
+const ApiContext = createContext<ApiContextValue | undefined>(undefined);
+
+export const ApiProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ApiContext.Provider value={{ apis: apiEndpoints }}>
+      {children}
+    </ApiContext.Provider>
+  );
+};
+
+export const useApis = () => {
+  const context = useContext(ApiContext);
+  if (!context) {
+    throw new Error('useApis must be used within an ApiProvider');
+  }
+  return context.apis;
+};
