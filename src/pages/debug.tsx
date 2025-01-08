06576ec8943af7661ecfadab617403be903640a2
rca <nem@nem.x0.to>
2025-01-08 12:46:14 +0900

    add debug page

---
+++ src/pages/debug.tsx
@@ -0,0 +1,11 @@
+import DefaultLayout from "@/layouts/default";
+
+export default function DebugPage() {
+  return (
+    <DefaultLayout>
+      <h1>
+        authed?
+      </h1>
+    </DefaultLayout>
+  );
+}
