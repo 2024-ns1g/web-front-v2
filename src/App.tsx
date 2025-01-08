ca91d245e657219d29c2f317d84956d7894b5a50
rca <nem@nem.x0.to>
2025-01-08 13:09:58 +0900

    Revert "add router wip"
    This reverts commit c3728bf376d60b1aec542bd270a2f75a0492b672.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useHref, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
 import DocsPage from "@/pages/docs";
@@ -9,30 +9,24 @@ import { Login } from "./pages/auth/login";
 import { Register } from "./pages/auth/register";
 import DebugPage from "./pages/debug";
 import { RouteAuthGuard } from "./pages/router-auth-guard";
-import { NextUIProvider } from "@nextui-org/system";
 
 function App() {
-
-  const navigate = useNavigate();
-
   return (
-    <NextUIProvider navigate={navigate} useHref={useHref} >
-      <Routes>
-        <Route element={<IndexPage />} path="/" />
-        <Route element={<DocsPage />} path="/docs" />
-        <Route element={<PricingPage />} path="/pricing" />
-        <Route element={<BlogPage />} path="/blog" />
-        <Route element={<AboutPage />} path="/about" />
-        <Route path="/debug" element={
-          <RouteAuthGuard component={<DebugPage />}
-          />
-        } />
+    <Routes>
+      <Route element={<IndexPage />} path="/" />
+      <Route element={<DocsPage />} path="/docs" />
+      <Route element={<PricingPage />} path="/pricing" />
+      <Route element={<BlogPage />} path="/blog" />
+      <Route element={<AboutPage />} path="/about" />
+      <Route path="/debug" element={
+        <RouteAuthGuard component={<DebugPage />}
+        />
+      } />
 
 
-        <Route element={<Login />} path="/auth/login" />
-        <Route element={<Register />} path="/auth/register" />
-      </Routes>
-    </NextUIProvider>
+      <Route element={<Login />} path="/auth/login" />
+      <Route element={<Register />} path="/auth/register" />
+    </Routes>
   );
 }
 
