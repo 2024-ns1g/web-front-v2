7378a371d1e4f4974a51f18063c1743a28170b10
rca <nem@nem.x0.to>
2025-01-08 10:49:59 +0900

    add api wrapper wip

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -4,4 +4,22 @@ import axios from 'axios';
 const apiClient = axios.create({
   baseURL: envLoader(envKeys.apiHost, 'http://localhost:8080'),
   timeout: Number(envLoader(envKeys.defaultTimeout, '5000')),
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
+
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    if (!config.headers)
+      config.headers = {};
+
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default apiClient;
