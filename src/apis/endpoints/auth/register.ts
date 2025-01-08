f8a89f41de97987c8554e77037571b018468617f
rca <nem@nem.x0.to>
2025-01-08 11:58:55 +0900

    impl register wrapper

---
+++ src/apis/endpoints/auth/register.ts
@@ -0,0 +1,29 @@
+import { useApiClient } from "@/contexts/api-client-context";
+import { useLogger } from "@/hooks/use-logger";
+import { z } from "zod";
+
+const registerRequestSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
+
+export type RegisterRequest = z.infer<typeof registerRequestSchema>;
+
+export const register = async (params: RegisterRequest) => {
+  const apiClient = useApiClient();
+  const { log } = useLogger('api/register');
+
+  const response = await apiClient.post('/auth/register', params);
+
+  try {
+    const parsed = registerRequestSchema.parse(response.data);
+
+    return parsed;
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      log('Failed to parse response', error.errors, "ERROR");
+    } else {
+      log('Unknown error', error, "ERROR");
+    }
+  }
+}
