c55d88862d0feb4276b1b8df6ced3f022fddee33
rca <nem@nem.x0.to>
2025-01-08 12:44:23 +0900

    add auth provider

--- src/provider.tsx
+++ src/provider.tsx
@@ -5,6 +5,7 @@ import { useHref, useNavigate } from "react-router-dom";
 import ToastProvider from "./contexts/toast-context";
 import { ApiClientProvider } from "./contexts/api-client-context";
 import { ApiProvider } from "./contexts/api-context";
+import { AuthProvider } from "./contexts/auth-context";
 
 declare module "@react-types/shared" {
   interface RouterConfig {
@@ -18,11 +19,13 @@ export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider navigate={navigate} useHref={useHref}>
       <ToastProvider>
-        <ApiClientProvider>
-          <ApiProvider>
-            {children}
-          </ApiProvider>
-        </ApiClientProvider>
+        <AuthProvider>
+          <ApiClientProvider>
+            <ApiProvider>
+              {children}
+            </ApiProvider>
+          </ApiClientProvider>
+        </AuthProvider>
       </ToastProvider>
     </NextUIProvider>
   );
