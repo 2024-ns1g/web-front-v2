110edc4c44eee0404d001c727bb9d0b70780ba96
rca <nem@nem.x0.to>
2025-01-08 12:29:40 +0900

    apply layout

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { toFormikValidationSchema } from 'zod-formik-adapter';
 import { toast } from "react-toastify";
 import { useLogger } from "@/hooks/use-logger";
+import { AuthLayout } from "@/layouts/authLayout";
 
 export const Login = () => {
 
@@ -25,48 +26,50 @@ export const Login = () => {
   };
 
   return (
-    <>
-      <div className="text-center text-[25px] font-bold mb-6">ログイン</div>
+    <AuthLayout>
+      <>
+        <div className="text-center text-[25px] font-bold mb-6">ログイン</div>
 
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        validationSchema={toFormikValidationSchema(loginFormSchema)}
-        onSubmit={handleLogin}
-      >
-        {({ values, errors, touched, handleChange, handleSubmit }) => (
-          <>
-            <div className="flex flex-col w-1/2 gap-4 mb-4">
-              <Input
-                variant="bordered"
-                label="ユーザー名"
-                type="text"
-                value={values.username}
-                isInvalid={!!errors.username && !!touched.username}
-                errorMessage={errors.username}
-                onChange={handleChange("username")}
-              />
-              <Input
-                variant="bordered"
-                label="パスワード"
-                type="password"
-                value={values.password}
-                isInvalid={!!errors.password && !!touched.password}
-                errorMessage={errors.password}
-                onChange={handleChange("password")}
-              />
-            </div>
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          validationSchema={toFormikValidationSchema(loginFormSchema)}
+          onSubmit={handleLogin}
+        >
+          {({ values, errors, touched, handleChange, handleSubmit }) => (
+            <>
+              <div className="flex flex-col w-1/2 gap-4 mb-4">
+                <Input
+                  variant="bordered"
+                  label="ユーザー名"
+                  type="text"
+                  value={values.username}
+                  isInvalid={!!errors.username && !!touched.username}
+                  errorMessage={errors.username}
+                  onChange={handleChange("username")}
+                />
+                <Input
+                  variant="bordered"
+                  label="パスワード"
+                  type="password"
+                  value={values.password}
+                  isInvalid={!!errors.password && !!touched.password}
+                  errorMessage={errors.password}
+                  onChange={handleChange("password")}
+                />
+              </div>
 
-            <Button onPress={() => handleSubmit()} variant="flat" color="primary">
-              ログイン
-            </Button>
-          </>
-        )}
-      </Formik>
+              <Button onPress={() => handleSubmit()} variant="flat" color="primary">
+                ログイン
+              </Button>
+            </>
+          )}
+        </Formik>
 
-      <div className="font-light text-slate-400 mt-4 text-sm">
-        {/* <Link href="/register" className="font-bold">新規登録</Link> */}
-        <Link to="/register" className="font-bold"> 新規登録</Link>
-      </div>
-    </>
+        <div className="font-light text-slate-400 mt-4 text-sm">
+          {/* <Link href="/register" className="font-bold">新規登録</Link> */}
+          <Link to="/register" className="font-bold"> 新規登録</Link>
+        </div>
+      </>
+    </AuthLayout>
   );
 };
