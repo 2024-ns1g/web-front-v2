840576be1532509fbdb8c8bf54f1cbf394768de2
rca <nem@nem.x0.to>
2025-01-08 11:35:03 +0900

    remove log

--- src/hooks/use-logger.ts
+++ src/hooks/use-logger.ts
@@ -2,7 +2,6 @@
 
 import { useCallback } from "react";
 
-// カラー設定
 const colors: Record<string, string> = {
   prefix: "#4CAF50",
   log: "inherit",
@@ -11,7 +10,6 @@ const colors: Record<string, string> = {
   error: "#F20404",
 };
 
-// タイムスタンプを生成するヘルパー関数
 function getTimestamp(): string {
   const date = new Date();
   const hours = date.getHours().toString().padStart(2, "0");
@@ -21,13 +19,11 @@ function getTimestamp(): string {
   return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
-// useLogger フックの定義
 type LogFunction = (message: string, src?: any) => void;
 
 export function useLogger(prefix: string): { log: LogFunction } {
   const log = useCallback<LogFunction>(
     (message, src) => {
-      // 本番環境ではログを出力しない
       if (process.env.NODE_ENV === "production") {
         return;
       }
