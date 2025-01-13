27e84eacf9d24d948e8ddd5f0178c02744050132
rca <nem@nem.x0.to>
2025-01-14 02:18:42 +0900

    keep return type wip

---
+++ src/types/api-wrapper.ts
@@ -0,0 +1,15 @@
+// types/api-wrapper.ts
+import { apiEndpoints } from '@/apis/endpoints';
+
+// Utility type to transform API endpoints
+type ApiEndpoints = typeof apiEndpoints;
+
+// Helper type to transform each API function
+type TransformApiFunction<T> = T extends (client: any, log: any, params: infer P) => Promise<infer R>
+  ? (params: P) => Promise<R>
+  : T extends object
+  ? { [K in keyof T]: TransformApiFunction<T[K]> }
+  : T;
+
+// Final type for useApis return value
+export type UseApis = TransformApiFunction<ApiEndpoints>;
