a1380b25db53d50135f3b0379b2683c721018f28
rca <nem@nem.x0.to>
2025-01-14 01:08:14 +0900

    remove deprecated

--- src/pages/router-auth-guard.tsx
+++ src/pages/router-auth-guard.tsx
@@ -5,12 +5,11 @@ import { Navigate, useLocation } from "react-router-dom";
 
 type Props = {
   component: React.ReactNode;
-}
+};
 
-export const RouteAuthGuard: React.VFC<Props> = (props) => {
+export const RouteAuthGuard = (props: Props) => {
   const auth = useAuth();
   const log = useLogger("RouteAuthGuard");
-
 
   let allowRoute = false;
   if (!auth.isAuthenticated) {
@@ -23,9 +22,8 @@ export const RouteAuthGuard: React.VFC<Props> = (props) => {
 
   if (!allowRoute) {
     // return <Navigate to={props.redirect} state={{from:useLocation()}} replace={false} />
-    return <Navigate to="/auth/login" state={{ from: useLocation() }} replace={false} />
+    return <Navigate to="/auth/login" state={{ from: useLocation() }} replace={false} />;
   } else log.info("Authenticated");
 
   return <>{props.component}</>;
-
-}
+};
