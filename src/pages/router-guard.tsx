6f6ce216528d6e8335625c259c9e27c9f07a8422
rca <nem@nem.x0.to>
2025-01-14 01:36:21 +0900

    improve

--- src/pages/router-guard.tsx
+++ src/pages/router-guard.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "@/contexts/auth-context";
 import { useLogger } from "@/hooks/use-logger";
+
+type AccessCheck = {
+  check: () => boolean;
+  redirectPath?: string;
+};
 
 type Props = {
   component: React.ReactNode;
-  checkAccess: Array<(auth: ReturnType<typeof useAuth>) => boolean>; // コールバックの配列
-  redirectPath?: string; // リダイレクト先のパス (デフォルト値: "/auth/login")
+  checkAccess: AccessCheck[]; // チェック条件の配列
+  defaultRedirectPath?: string; // 全体のデフォルトリダイレクト先
 };
 
-export const RouterGuard: React.FC<Props> = ({ component, checkAccess, redirectPath = "/auth/login" }) => {
-  const auth = useAuth();
+export const RouteAuthGuard: React.FC<Props> = ({ component, checkAccess, defaultRedirectPath = "/auth/login" }) => {
   const log = useLogger("RouteAuthGuard");
   const location = useLocation();
 
-  // 全ての条件を評価
-  const allowRoute = checkAccess.every((check) => check(auth));
-
-  if (!allowRoute) {
-    log.info("Access denied. Redirecting to login.");
-    return <Navigate to={redirectPath} state={{ from: location }} replace={false} />;
+  for (const { check, redirectPath } of checkAccess) {
+    if (!check()) {
+      const targetPath = redirectPath || defaultRedirectPath; // 指定されたリダイレクト先かデフォルトを使用
+      log.info(`Access denied. Redirecting to ${targetPath}.`);
+      return <Navigate to={targetPath} state={{ from: location }} replace={false} />;
+    }
   }
 
   log.info("Access granted.");
