d0a7a4703da5fa195e16f43330a361dc9c1f159d
rca <nem@nem.x0.to>
2025-01-14 03:24:12 +0900

    wip

--- src/apis/endpoints/slide/createSlide.ts
+++ src/apis/endpoints/slide/createSlide.ts
@@ -1,9 +1,14 @@
+import { StateContextType } from "@/contexts/state-context";
 import { CreateSlideRequest, createSlideResponseSchema } from "@/types/endpoints/slide/createSlide";
 import { AxiosInstance } from "axios";
 import { z } from "zod";
 
-export const createSlide = async (apiClient: AxiosInstance, log: any, params: CreateSlideRequest) => {
-  const response = await apiClient.post('/room/create', params);
+export const createSlide = async (apiClient: AxiosInstance, log: any, state: StateContextType, params: CreateSlideRequest) => {
+  if (!state.activeRoomId) {
+    log.error('activeRoomId is not set');
+    return;
+  }
+  const response = await apiClient.post(`/room/${state.activeRoomId}/slide/create`, params);
   try {
     const parsed = createSlideResponseSchema.parse(response.data);
     return parsed;
