87f1364952aec3241c0cb14c0e3c0d9f5eb96451
rca <nem@nem.x0.to>
2025-01-15 15:18:07 +0900

    impl audience context

---
+++ src/contexts/audience-context.tsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState } from "react";
+
+type AudienceContextType = {
+  joinedSessionId: string;
+  setJoinedSessionId: (id: string) => void;
+}
+
+const AudienceContext = createContext<AudienceContextType>({
+  joinedSessionId: "",
+  setJoinedSessionId: () => { }
+});
+
+interface AudienceProviderProps {
+  children: React.ReactNode;
+}
+
+export const AudienceProvider = ({ children }: AudienceProviderProps) => {
+  const [joinedSessionId, setJoinedSessionId] = useState("");
+  return (
+    <AudienceContext.Provider value={{ joinedSessionId, setJoinedSessionId }}>
+      {children}
+    </AudienceContext.Provider>
+  );
+}
+
+export const useAudienceContext = () => {
+  const context = useContext(AudienceContext);
+  if (!context) {
+    throw new Error("useAudienceContext must be used within a AudienceProvider");
+  }
+}
+
