2214fc32021c9cd39ac0a03e425b31156fd15578
rca <nem@nem.x0.to>
2025-01-15 14:38:28 +0900

    test

--- src/pages/script-editor.tsx
+++ src/pages/script-editor.tsx
@@ -1,4 +1,5 @@
 import { useLogger } from "@/hooks/use-logger";
+import DefaultLayout from "@/layouts/default";
 import { Button } from "@nextui-org/button";
 import { Switch } from "@nextui-org/switch";
 import MDEditor from "@uiw/react-md-editor";
@@ -19,24 +20,26 @@ export default function ScriptEditor() {
   };
 
   return (
-    <div className="h-full flex flex-col">
-      <MDEditor
-        className="flex-grow"
-        height="100%"
-        value={value}
-        onChange={handleChange}
-      />
-      <div className="flex justify-between p-2 x-2">
-        <div>
-          <Switch isSelected={autoSave} onValueChange={setAutoSave}>
-            自動保存
-          </Switch>
-        </div>
-        <div className="space-x-2">
-          <Button color="default" variant="bordered" size="sm">キャンセル</Button>
-          <Button color="primary" size="sm" onPress={() => handleSave()}>保存</Button>
+    <DefaultLayout>
+      <div className="h-full flex flex-col">
+        <MDEditor
+          className="flex-grow"
+          height="100%"
+          value={value}
+          onChange={handleChange}
+        />
+        <div className="flex justify-between p-2 x-2">
+          <div>
+            <Switch isSelected={autoSave} onValueChange={setAutoSave}>
+              自動保存
+            </Switch>
+          </div>
+          <div className="space-x-2">
+            <Button color="default" variant="bordered" size="sm">キャンセル</Button>
+            <Button color="primary" size="sm" onPress={() => handleSave()}>保存</Button>
+          </div>
         </div>
       </div>
-    </div>
+    </DefaultLayout>
   );
 }
