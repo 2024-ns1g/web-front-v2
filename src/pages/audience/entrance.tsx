ac631bbf032d2027cf0a6b967e2bb23e4c378cd0
rca <nem@nem.x0.to>
2025-01-17 09:41:28 +0900

    add announce wip

--- src/pages/audience/entrance.tsx
+++ src/pages/audience/entrance.tsx
@@ -1,8 +1,10 @@
 import { useAudienceContext } from "@/contexts/audience-context";
 import { InputOtp } from "@nextui-org/react";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AudienceEntrancePage() {
+  const navigate = useNavigate();
 
   const audience = useAudienceContext();
 
@@ -21,12 +23,15 @@ export default function AudienceEntrancePage() {
 
   const handleWorkaroundSubmit = () => {
     audience.setJoinedSessionId(sessionId);
+
+    navigate("/audience");
   };
 
   return (
     <>
       <div className="fixed inset-0 bg-black bg-opacity-50 z-50">
         {/* 暫定: セッションIDを直接入力 */}
+        <p className="text-white text-xl w-full text-center mt-14">Workaround Overlay</p>
         <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-lg">
           <p className="text-center text-lg">セッションIDを入力してください。</p>
           <input type="text" onChange={handleSessionIdChange} className="w-full p-2 border border-gray-300 rounded mt-2" />
