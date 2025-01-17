9af597b60e4f5668f7f38fada6886e03ffb7b2eb
rca <nem@nem.x0.to>
2025-01-17 09:34:54 +0900

    replace import

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,3 @@
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
-import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
 import {
   Navbar as NextUINavbar,
   NavbarBrand,
@@ -10,7 +6,11 @@ import {
   NavbarMenuToggle,
   NavbarMenu,
   NavbarMenuItem,
-} from "@nextui-org/navbar";
+  Input,
+  Kbd,
+  Link,
+  Button,
+} from "@nextui-org/react";
 import { link as linkStyles } from "@nextui-org/theme";
 import clsx from "clsx";
 
