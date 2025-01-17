79d174ce66c7a641f64b3d7ea2a27e4ba69fdc33
rca <nem@nem.x0.to>
2025-01-17 09:31:22 +0900

    replace import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
+import { Code, Link, Snippet } from "@nextui-org/react";
 
 export default function IndexPage() {
   return (
