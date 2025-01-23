114260984818efac8c0f6dcfd84bdc806088c9c8
rca <nem@nem.x0.to>
2025-01-24 00:47:25 +0900

    drawer test

--- src/pages/audience/index.tsx
+++ src/pages/audience/index.tsx
@@ -1,9 +1,13 @@
 import { Header } from "@/components/audience/header";
 import { useAudienceContext } from "@/contexts/audience-context";
+import { Button, Drawer, DrawerContent, DrawerFooter, DrawerHeader } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 
 export default function AudienceIndexPage() {
   const audience = useAudienceContext();
+
+  // State
+  const [isVoteDrawerOpen, setIsVoteDrawerOpen] = useState(false);
 
   // debug, when the page is loaded, show the session info
   useEffect(() => {
@@ -14,11 +18,31 @@ export default function AudienceIndexPage() {
 
   return (
     <>
-      <Header 
-        totalSlides={10} 
-        currentSlideIndex={3} 
-        sessionName="Session Name" 
+      <Header
+        totalSlides={10}
+        currentSlideIndex={3}
+        sessionName="Session Name"
         currentSlideName="Current Slide Name" />
+
+      <Button color="primary" onClick={() => setIsVoteDrawerOpen(true)}>test</Button>
+
+      <Drawer isOpen={isVoteDrawerOpen} >
+        <DrawerContent>
+          {(onClose) => (
+            <>
+              <DrawerHeader>てすと</DrawerHeader>
+              <DrawerFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </DrawerFooter>
+            </>
+          )}
+        </DrawerContent>
+      </Drawer>
     </>
   );
 }
