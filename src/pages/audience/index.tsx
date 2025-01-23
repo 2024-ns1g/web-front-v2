3adab79c3fa5b4b7c5cb3f798423f522cf8434f5
rca <nem@nem.x0.to>
2025-01-23 22:49:05 +0900

    debug

--- src/pages/audience/index.tsx
+++ src/pages/audience/index.tsx
@@ -3,8 +3,14 @@ import { Card, Tab, Tabs } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 
 export default function AudienceIndexPage() {
-
   const audience = useAudienceContext();
+
+  // debug, when the page is loaded, show the session info
+  useEffect(() => {
+    audience.sessionInfo.then((info) => {
+      console.log(info);
+    });
+  }, []);
 
   return (
     <>
