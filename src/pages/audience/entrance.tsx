9a87ed3f532b22b4c12e7cfa3ca3077bac1f277b
rca <nem@nem.x0.to>
2025-01-23 18:42:50 +0900

    remove workaround

--- src/pages/audience/entrance.tsx
+++ src/pages/audience/entrance.tsx
@@ -25,14 +25,6 @@ export default function AudienceEntrancePage() {
 
   return (
     <>
-      <div className="fixed inset-0 bg-black bg-opacity-50 z-50">
-        {/* 暫定: セッションIDを直接入力 */}
-        <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-lg">
-          <p className="text-center text-lg">セッションIDを入力してください。</p>
-          <input type="text" onChange={handleSessionIdChange} className="w-full p-2 border border-gray-300 rounded mt-2" />
-          <button className="w-full p-2 bg-gray-800 text-white rounded mt-2" onClick={handleWorkaroundSubmit}>Submit</button>
-        </div>
-      </div>
       <div className="flex flex-col items-center justify-center h-full">
         <div>
           <p className="text-center text-xl">認証コード</p>
