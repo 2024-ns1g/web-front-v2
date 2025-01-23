e082829cf0db239b1778ed3320570631157cbe53
rca <nem@nem.x0.to>
2025-01-24 04:46:17 +0900

    use page selector

--- src/pages/audience/index.tsx
+++ src/pages/audience/index.tsx
@@ -1,5 +1,6 @@
 import { BodyMarkdownViewer } from "@/components/audience/body/markdown-viewer";
 import { Header } from "@/components/audience/header";
+import { BodyPageSelector } from "@/components/audience/tab-selector";
 import { VoteDrawerBody } from "@/components/audience/vote";
 import { useAudienceContext } from "@/contexts/audience-context";
 import { SessionInfo } from "@/types/audience/session-info-schema";
@@ -15,6 +16,8 @@ export default function AudienceIndexPage() {
   const [sessionInfo, setSessionInfo] = useState<SessionInfo | null>(null);
 
   const [state, setState] = useState(audience.state);
+
+  const [selectedPageId, setSelectedPageId] = useState<string | null>(null);
 
   useEffect(() => {
     audience.sessionInfo.then((info) => {
@@ -38,187 +41,31 @@ export default function AudienceIndexPage() {
     {
       pageId: "1",
       title: "Page 1",
+      scripts: [
+        {
+          content: "# Page 1\n\nThis is page 1.",
+        },
+      ],
     },
     {
       pageId: "2",
       title: "Page 2",
+      scripts: [
+        {
+          content: "# Page 2\n\nThis is page 2.",
+        },
+      ],
     },
     {
       pageId: "3",
       title: "Page 3",
+      scripts: [
+        {
+          content: "# Page 3\n\nThis is page 3.",
+        },
+      ],
     },
-    {
-      pageId: "4",
-      title: "Page 4",
-    },
-    {
-      pageId: "5",
-      title: "Page 5",
-    },
-    {
-      pageId: "6",
-      title: "Page 6",
-    },
-    {
-      pageId: "7",
-      title: "Page 7",
-    },
-    {
-      pageId: "8",
-      title: "Page 8",
-    },
-    {
-      pageId: "9",
-      title: "Page 9",
-    },
-    {
-      pageId: "10",
-      title: "Page 10",
-    },
-    {
-      pageId: "11",
-      title: "Page 11",
-    },
-    {
-      pageId: "12",
-      title: "Page 12",
-    },
-    {
-      pageId: "13",
-      title: "Page 13",
-    },
-    {
-      pageId: "14",
-      title: "Page 14",
-    },
-    {
-      pageId: "15",
-      title: "Page 15",
-    },
-    {
-      pageId: "16",
-      title: "Page 16",
-    },
-    {
-      pageId: "17",
-      title: "Page 17",
-    },
-    {
-      pageId: "18",
-      title: "Page 18",
-    },
-    {
-      pageId: "19",
-      title: "Page 19",
-    },
-    {
-      pageId: "20",
-      title: "Page 20",
-    },
-    {
-      pageId: "21",
-      title: "Page 21",
-    },
-    {
-      pageId: "22",
-      title: "Page 22",
-    },
-    {
-      pageId: "23",
-      title: "Page 23",
-    },
-    {
-      pageId: "24",
-      title: "Page 24",
-    },
-    {
-      pageId: "25",
-      title: "Page 25",
-    },
-    {
-      pageId: "26",
-      title: "Page 26",
-    },
-    {
-      pageId: "27",
-      title: "Page 27",
-    },
-    {
-      pageId: "28",
-      title: "Page 28",
-    },
-    {
-      pageId: "29",
-      title: "Page 29",
-    },
-    {
-      pageId: "30",
-      title: "Page 30",
-    },
-    {
-      pageId: "31",
-      title: "Page 31",
-    },
-    {
-      pageId: "32",
-      title: "Page 32",
-    },
-    {
-      pageId: "33",
-      title: "Page 33",
-    },
-    {
-      pageId: "34",
-      title: "Page 34",
-    },
-    {
-      pageId: "35",
-      title: "Page 35",
-    },
-    {
-      pageId: "36",
-      title: "Page 36",
-    },
-    {
-      pageId: "37",
-      title: "Page 37",
-    },
-    {
-      pageId: "38",
-      title: "Page 38",
-    },
-    {
-      pageId: "39",
-      title: "Page 39",
-    },
-    {
-      pageId: "40",
-      title: "Page 40",
-    },
-    {
-      pageId: "41",
-      title: "Page 41",
-    },
-    {
-      pageId: "42",
-      title: "Page 42",
-    },
-    {
-      pageId: "43",
-      title: "Page 43",
-    },
-    {
-      pageId: "44",
-      title: "Page 44",
-    },
-    {
-      pageId: "45",
-      title: "Page 45",
-    },
-    {
-      pageId: "46",
-      title: "Page 46",
-    },
+
   ]
 
   return (
@@ -254,83 +101,12 @@ export default function AudienceIndexPage() {
         </DrawerContent>
       </Drawer>
 
-      <div className="flex-grow flex items-center justify-center">
-        <div className="container h-full px-4 py-8">
+      <div className="flex-grow h-full">
+        <BodyPageSelector defaultPageIndex={0} onSelectedPageChanged={(pageId) => {setSelectedPageId(pageId)}} pages={debugPages} />
 
-          <Tabs items={debugPages ?? []} classNames={{ tabList: "bg-blue-500", tab: "bg-red-500", tabContent: "h-full", panel: "h-full" }}>
-            {(item) => (
-              <Tab key={item.pageId} title={item.title}>
-                <div className="h-full">
-                  {/* <BodyMarkdownViewer content={item.scripts[0].content} /> */}
-                  <BodyMarkdownViewer content={`
-# あああああああああああ
-## あああああ
-ああ
-
-あああ
-
-ｓださｄｄだｄｓｄ
-
-ｆｄｓｆｓｄｆｓｄｆ
-
-
-ｆｄｓｆｓｄｆｄｆｓ
-
-
-ｆｓｄｆｓｄｆｓｄｆｓｆ
-
-
-
-
-ｆｄｓｆｄｆｓｆｄｆ
-# あああああああああああ
-## あああああ
-ああ
-
-あああ
-
-ｓださｄｄだｄｓｄ
-
-ｆｄｓｆｓｄｆｓｄｆ
-
-
-ｆｄｓｆｓｄｆｄｆｓ
-
-
-ｆｓｄｆｓｄｆｓｄｆｓｆ
-
-
-
-
-ｆｄｓｆｄｆｓｆｄｆ
-# あああああああああああ
-## あああああ
-ああ
-
-あああ
-
-ｓださｄｄだｄｓｄ
-
-ｆｄｓｆｓｄｆｓｄｆ
-
-
-ｆｄｓｆｓｄｆｄｆｓ
-
-
-ｆｓｄｆｓｄｆｓｄｆｓｆ
-
-
-
-
-ｆｄｓｆｄｆｓｆｄｆ
-                    `}/>
-                </div>
-              </Tab>
-            )}
-          </Tabs>
-        </div>
+        <BodyMarkdownViewer content={debugPages.find((p) => p.pageId === selectedPageId)?.scripts[0].content ?? ""} />
       </div>
-    </div>
 
+    </div>
   );
 }
