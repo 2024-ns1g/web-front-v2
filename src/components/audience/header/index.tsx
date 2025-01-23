e1d32c75c8837729f86644c587d2e6a03f768522
rca <nem@nem.x0.to>
2025-01-23 23:24:54 +0900

    add actions, style wip

--- src/components/audience/header/index.tsx
+++ src/components/audience/header/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { HeaderProgressBar } from "./progress";
 import { HeaderBreadcrumb } from "./breadcrumb";
+import { HeaderActions } from "./actions";
 
 interface HeaderProps {
   totalSlides: number;
@@ -17,8 +18,15 @@ export const Header: FC<HeaderProps> = ({
 }) => {
   return (
     <>
-      <HeaderBreadcrumb sessionName={sessionName} currentSlideName={currentSlideName} />
-      <HeaderProgressBar totalSlides={totalSlides} currentSlideIndex={currentSlideIndex} />
+      <div className="flex flex-row gap-4 w-full justify-center px-4">
+        <div className="container flex flex-row">
+          <div className="flex flex-col flex-grow">
+            <HeaderBreadcrumb sessionName={sessionName} currentSlideName={currentSlideName} />
+            <HeaderProgressBar totalSlides={totalSlides} currentSlideIndex={currentSlideIndex} />
+          </div>
+          <HeaderActions isWsConnected={true} voteCount={0} wsClickedHandler={() => { }} voteClickedHandler={() => { }} />
+        </div>
+      </div>
     </>
   );
 };
