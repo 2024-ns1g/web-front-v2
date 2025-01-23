41697bb2348e3e8222204c7e5021aef9afad0173
rca <nem@nem.x0.to>
2025-01-23 18:50:43 +0900

    impl logic wip

--- src/pages/audience/entrance.tsx
+++ src/pages/audience/entrance.tsx
@@ -1,14 +1,26 @@
 import { useAudienceContext } from "@/contexts/audience-context";
+import { useApis } from "@/hooks/use-api";
 import { Button, InputOtp } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 
 export default function AudienceEntrancePage() {
 
+  const api = useApis();
   const audience = useAudienceContext();
 
   const [value, setValue] = useState("");
 
   const handleJoinToSession = () => {
+    api.session.audience.verifyAudienceOtp({
+      otp: value
+    }).then((response) => {
+      if (!response || !response.sessionId) { // TODO: Improve handling
+        console.log("OTP verification failed");
+        return
+      }
+      console.log("OTP verified");
+      audience.setJoinedSessionId(response.sessionId);
+    });
   }
 
   return (
