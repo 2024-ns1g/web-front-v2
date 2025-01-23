736acf8a4b54eda10d2a60f3119c6c26c68c0636
rca <nem@nem.x0.to>
2025-01-24 03:04:17 +0900

    add markdown viewer component wip

---
+++ src/components/audience/body/markdown-viewer.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import Markdown from "react-markdown";
+
+interface BodyMarkdownViewerProps {
+  content: string;
+};
+
+export const BodyMarkdownViewer: FC<BodyMarkdownViewerProps> = ({
+  content
+}) => {
+  return (
+    <Markdown>{content}</Markdown>
+  );
+};
