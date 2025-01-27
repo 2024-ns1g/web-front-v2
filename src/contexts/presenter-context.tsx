5ad60671c324b0e35dad36655d954c2a7ea79c51
rca <nem@nem.x0.to>
2025-01-28 03:16:28 +0900

    add presenter context wip

---
+++ src/contexts/presenter-context.tsx
@@ -0,0 +1,26 @@
+import { createContext, useContext } from "react";
+
+type PresenterContextType = {
+};
+
+const PresenterContext = createContext<PresenterContextType | null>(null);
+
+interface PresenterProviderProps {
+  children: React.ReactNode;
+};
+
+export const PresenterProvider = ({ children }: PresenterProviderProps) => {
+  return (
+    <PresenterContext.Provider value={{}}>
+      {children}
+    </PresenterContext.Provider>
+  );
+}
+
+export const usePresenter = () => {
+  const context = useContext(PresenterContext);
+  if (!context) {
+    throw new Error("usePresenter must be used within a PresenterProvider");
+  }
+  return context;
+}
