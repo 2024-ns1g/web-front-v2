ab2b28401113d2cd276bb51770e60f38c8394555
rca <nem@nem.x0.to>
2025-01-28 03:27:45 +0900

    add votes

--- src/types/session/session-state.ts
+++ src/types/session/session-state.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
+import { VoteSchema } from "./vote";
 
 export const SessionStateSchema = z.object({
   currentPage: z.number(),
   activeVoteIds: z.array(z.string()),
+  votes: z.array(VoteSchema),
 });
 
 export type SessionState = z.infer<typeof SessionStateSchema>;
