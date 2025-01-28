1488ff7d8cc75741604855654f46938c801f216a
rca <nem@nem.x0.to>
2025-01-29 01:39:54 +0900

    wip

---
+++ src/types/session/presenter-settings.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+
+// ジェスチャーのラベルとキーの対応
+export const GestureClassification = {
+  Unknown: {
+    key: 0,
+    displayName: '不明',
+  },
+  Closed_Fist: {
+    key: 1,
+    displayName: '✊',
+  },
+  Open_Palm: {
+    key: 2,
+    displayName: '🖐️',
+  },
+  Pointing_Up: {
+    key: 3,
+    displayName: '☝️',
+  },
+  Thumbs_Down: {
+    key: 4,
+    displayName: '👎',
+  },
+  Thumbs_Up: {
+    key: 5,
+    displayName: '👍',
+  },
+  Victory: {
+    key: 6,
+    displayName: '✌️',
+  },
+  ILoveYou: {
+    key: 7,
+    displayName: '🤟',
+  },
+} as const;
+
+const PresenterSettingsSchema = z.object({
+  const gesture = z.object({
+    threshold: z.number(),
+    stableTime: z.number(),
+    actionMap: z.record(z.object({
+      gesture: z.union([
+        z.literal(GestureClassification.Closed_Fist.key),
+        z.literal(GestureClassification.Open_Palm.key),
+        z.literal(GestureClassification.Pointing_Up.key),
+        z.literal(GestureClassification.Thumbs_Down.key),
+        z.literal(GestureClassification.Thumbs_Up.key),
+        z.literal(GestureClassification.Victory.key),
+        z.literal(GestureClassification.ILoveYou.key),
+      ]),
+      action: z.string(),
+    })),
+
