60b556b0ae67e0f9ee2fd0f45845b46d855df621
rca <nem@nem.x0.to>
2025-01-30 03:15:03 +0900

    wip

---
+++ src/components/presenter/blocks/seekSlide.tsx
@@ -0,0 +1,19 @@
+// ボタンで前後のイベントをトリガーする(=矢印キーの動作)コンポーネント
+
+import { FC } from "react";
+
+type PresenterBlockSeekSlideProps = {
+  canSeekPrev: boolean;
+  canSeekNext: boolean;
+  onSeekPrev: () => void;
+  onSeekNext: () => void;
+};
+
+export const PresenterBlockSeekSlide: FC<PresenterBlockSeekSlideProps> = ({
+}) => {
+  return (
+    <>
+      <p>Placeholder</p>
+    </>
+  );
+}
