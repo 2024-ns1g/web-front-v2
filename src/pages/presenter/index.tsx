1a516045972e3124a4742812f6b255b5ae525ac2
rca <nem@nem.x0.to>
2025-01-30 03:34:14 +0900

    debug add block

--- src/pages/presenter/index.tsx
+++ src/pages/presenter/index.tsx
@@ -1,8 +1,16 @@
+import { PresenterBlockDirectSeekSlide } from "@/components/presenter/blocks/directSeekSlide";
+
 export default function PresenterIndexPage() {
 
   return (
     <>
       <h1 className="text-4xl">Presenter</h1>
+      <PresenterBlockDirectSeekSlide
+        canSeekPrev={false}
+        canSeekNext={false}
+        onSeekPrev={() => { }}
+        onSeekNext={() => { }}
+      ></PresenterBlockDirectSeekSlide>
     </>
   );
 }
