d7b899830c179b26ed65074c56275bcf825a5ff5
rca <nem@nem.x0.to>
2025-02-01 14:20:27 +0900

    test

--- src/pages/presenter/index.tsx
+++ src/pages/presenter/index.tsx
@@ -1,16 +1,57 @@
 import { PresenterBlockDirectSeekSlide } from "@/components/presenter/blocks/directSeekSlide";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 
 export default function PresenterIndexPage() {
 
   return (
     <>
+      <ResponsiveMasonry>
+        <Masonry>
+          <PresenterBlockDirectSeekSlide
+            canSeekPrev={false}
+            canSeekNext={true}
+            onSeekPrev={() => { }}
+            onSeekNext={() => { }}
+          ></PresenterBlockDirectSeekSlide>
+          <PresenterBlockDirectSeekSlide
+            canSeekPrev={false}
+            canSeekNext={true}
+            onSeekPrev={() => { }}
+            onSeekNext={() => { }}
+          ></PresenterBlockDirectSeekSlide>
+          <PresenterBlockDirectSeekSlide
+            canSeekPrev={false}
+            canSeekNext={true}
+            onSeekPrev={() => { }}
+            onSeekNext={() => { }}
+          ></PresenterBlockDirectSeekSlide>
+          <PresenterBlockDirectSeekSlide
+            canSeekPrev={false}
+            canSeekNext={true}
+            onSeekPrev={() => { }}
+            onSeekNext={() => { }}
+          ></PresenterBlockDirectSeekSlide>
+          <PresenterBlockDirectSeekSlide
+            canSeekPrev={false}
+            canSeekNext={true}
+            onSeekPrev={() => { }}
+            onSeekNext={() => { }}
+          ></PresenterBlockDirectSeekSlide>
+          <PresenterBlockDirectSeekSlide
+            canSeekPrev={false}
+            canSeekNext={true}
+            onSeekPrev={() => { }}
+            onSeekNext={() => { }}
+          ></PresenterBlockDirectSeekSlide>
+          <PresenterBlockDirectSeekSlide
+            canSeekPrev={false}
+            canSeekNext={true}
+            onSeekPrev={() => { }}
+            onSeekNext={() => { }}
+          ></PresenterBlockDirectSeekSlide>
+        </Masonry>
+      </ResponsiveMasonry>
       <h1 className="text-4xl">Presenter</h1>
-      <PresenterBlockDirectSeekSlide
-        canSeekPrev={false}
-        canSeekNext={true}
-        onSeekPrev={() => { }}
-        onSeekNext={() => { }}
-      ></PresenterBlockDirectSeekSlide>
     </>
   );
 }
