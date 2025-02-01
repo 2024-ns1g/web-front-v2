f2fcaeb4689924bc7834ac92ff8d0df20e080879
rca <nem@nem.x0.to>
2025-02-02 05:22:33 +0900

    block tmp

---
+++ src/components/presenter/blocks/presentationStatus.tsx
@@ -0,0 +1,33 @@
+// プレゼンの進行状態を表示するコンポーネント
+
+import { Button, Card, CardBody } from "@nextui-org/react";
+import { FC } from "react";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+
+type PresenterBlockSessionStatusProps = {
+  slideTitle: string;
+  totalPageNumber: number;
+  currentPageIndexNumber: number;
+  currentPageTitle: string;
+};
+
+export const PresenterBlockSessionStatus: FC<PresenterBlockSessionStatusProps> = ({
+  slideTitle,
+  totalPageNumber,
+  currentPageIndexNumber,
+  currentPageTitle
+}) => {
+  return (
+    <Card className="w-full">
+      <CardBody>
+        <div className="flex justify-between">
+          <div>
+            <p>{slideTitle}</p>
+            <p>{currentPageIndexNumber} / {totalPageNumber}</p>
+          </div>
+          <p>{currentPageTitle}</p>
+        </div>
+      </CardBody>
+    </Card>
+  );
+};
--- src/pages/presenter/index.tsx
+++ src/pages/presenter/index.tsx
@@ -1,12 +1,14 @@
 import { PresenterBlockDirectSeekSlide } from "@/components/presenter/blocks/directSeekSlide";
+import { PresenterBlockSessionStatus } from "@/components/presenter/blocks/presentationStatus";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 
 export default function PresenterIndexPage() {
 
   return (
     <>
+      <h1 className="text-4xl">Presenter</h1>
       <ResponsiveMasonry
-        className="flex justify-center">
+        className="flex justify-center px-4">
         <Masonry
           className="container"
         >
@@ -16,45 +18,14 @@ export default function PresenterIndexPage() {
             onSeekPrev={() => { }}
             onSeekNext={() => { }}
           ></PresenterBlockDirectSeekSlide>
-          <PresenterBlockDirectSeekSlide
-            canSeekPrev={false}
-            canSeekNext={true}
-            onSeekPrev={() => { }}
-            onSeekNext={() => { }}
-          ></PresenterBlockDirectSeekSlide>
-          <PresenterBlockDirectSeekSlide
-            canSeekPrev={false}
-            canSeekNext={true}
-            onSeekPrev={() => { }}
-            onSeekNext={() => { }}
-          ></PresenterBlockDirectSeekSlide>
-          <PresenterBlockDirectSeekSlide
-            canSeekPrev={false}
-            canSeekNext={true}
-            onSeekPrev={() => { }}
-            onSeekNext={() => { }}
-          ></PresenterBlockDirectSeekSlide>
-          <PresenterBlockDirectSeekSlide
-            canSeekPrev={false}
-            canSeekNext={true}
-            onSeekPrev={() => { }}
-            onSeekNext={() => { }}
-          ></PresenterBlockDirectSeekSlide>
-          <PresenterBlockDirectSeekSlide
-            canSeekPrev={false}
-            canSeekNext={true}
-            onSeekPrev={() => { }}
-            onSeekNext={() => { }}
-          ></PresenterBlockDirectSeekSlide>
-          <PresenterBlockDirectSeekSlide
-            canSeekPrev={false}
-            canSeekNext={true}
-            onSeekPrev={() => { }}
-            onSeekNext={() => { }}
-          ></PresenterBlockDirectSeekSlide>
+          <PresenterBlockSessionStatus
+            slideTitle="スライドタイトル"
+            totalPageNumber={10}
+            currentPageIndexNumber={1}
+            currentPageTitle="あああ"
+          ></PresenterBlockSessionStatus>
         </Masonry>
       </ResponsiveMasonry>
-      <h1 className="text-4xl">Presenter</h1>
     </>
   );
 }
