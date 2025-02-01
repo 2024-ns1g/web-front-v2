e548374206312cfa3904f5fbcbafb581081a04ae
rca <nem@nem.x0.to>
2025-02-02 07:11:09 +0900

    add key

--- src/types/session/session-state.ts
+++ src/types/session/session-state.ts
@@ -3,6 +3,7 @@ import { VoteSchema } from "./vote";
 
 export const SessionStateSchema = z.object({
   currentPage: z.number(),
+  currentStep: z.number(),
   activeVoteIds: z.array(z.string()),
   votes: z.array(VoteSchema),
 });
