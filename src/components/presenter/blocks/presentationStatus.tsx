eb048b3e576275a6d4831962ed3ffa42e4d209e3
rca <nem@nem.x0.to>
2025-02-02 07:37:42 +0900

    remake wip

--- src/components/presenter/blocks/presentationStatus.tsx
+++ src/components/presenter/blocks/presentationStatus.tsx
@@ -1,6 +1,6 @@
 // プレゼンの進行状態を表示するコンポーネント
 
-import { Button, Card, CardBody } from "@nextui-org/react";
+import { Button, Card, CardBody, Progress } from "@nextui-org/react";
 import { FC } from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
@@ -24,12 +24,13 @@ export const PresenterBlockSessionStatus: FC<PresenterBlockSessionStatusProps> =
   return (
     <Card className="w-full">
       <CardBody>
-        <div className="flex justify-between">
-          <div>
-            <p>{slideTitle}</p>
-            <p>{currentPageIndexNumber} / {totalPageNumber}</p>
-          </div>
-          <p>{currentPageTitle}</p>
+        <div>
+          <Progress value={(currentPageIndexNumber / totalPageNumber) * 100} />
+        </div>
+        <div className="flex items-center pt-3">
+          <p className="text-sm text-gray-600">{slideTitle}</p>
+          <FaAngleRight size={12}/>
+          <p className="text-lg text-gray-900">{currentPageTitle}</p>
         </div>
       </CardBody>
     </Card>
