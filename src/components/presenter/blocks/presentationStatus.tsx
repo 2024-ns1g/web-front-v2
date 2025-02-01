8778ab41a5bf907486bc39fa17d70213f21645c0
rca <nem@nem.x0.to>
2025-02-02 08:45:26 +0900

    add step progress wip

--- src/components/presenter/blocks/presentationStatus.tsx
+++ src/components/presenter/blocks/presentationStatus.tsx
@@ -24,12 +24,22 @@ export const PresenterBlockSessionStatus: FC<PresenterBlockSessionStatusProps> =
   return (
     <Card className="w-full">
       <CardBody>
-        <div>
-          <Progress value={(currentPageIndexNumber / totalPageNumber) * 100} />
+        <div className="flex flex-row items-center">
+          <Progress className="flex-grow" value={(currentPageIndexNumber / totalPageNumber) * 100} />
+          <div className="flex flex-row flex-shrink-0 pl-3">
+            <p className="text-sm text-gray-600">{currentPageIndexNumber} / {totalPageNumber}</p>
+          </div>
         </div>
+        <div className="flex flex-row items-center pt-3">
+          <Progress className="flex-grow" value={(currentPageCurrentStepNumber / currentPageTotalStepNumber) * 100} />
+          <div className="flex flex-row flex-shrink-0 pl-3">
+            <p className="text-sm text-gray-600">{currentPageCurrentStepNumber} / {currentPageTotalStepNumber}</p>
+          </div>
+        </div>
+
         <div className="flex items-center pt-3">
           <p className="text-sm text-gray-600">{slideTitle}</p>
-          <FaAngleRight size={12}/>
+          <FaAngleRight size={12} />
           <p className="text-lg text-gray-900">{currentPageTitle}</p>
         </div>
       </CardBody>
