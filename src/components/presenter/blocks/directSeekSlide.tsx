7b9d0846735b2ae5522b4bbae38258034f7c0dce
rca <nem@nem.x0.to>
2025-02-02 05:54:09 +0900

    fix seek block wip

--- src/components/presenter/blocks/directSeekSlide.tsx
+++ src/components/presenter/blocks/directSeekSlide.tsx
@@ -2,7 +2,7 @@
 
 import { Button, Card, CardBody } from "@nextui-org/react";
 import { FC } from "react";
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import { FaAngleLeft, FaAngleRight, FaAnglesLeft, FaAnglesRight } from "react-icons/fa6";
 
 type PresenterBlockDirectSeekSlideProps = {
   canSeekPrev: boolean;
@@ -21,12 +21,25 @@ export const PresenterBlockDirectSeekSlide: FC<PresenterBlockDirectSeekSlideProp
     <Card className="w-full">
       <CardBody>
         <div className="flex justify-between">
-          <Button isIconOnly disabled={!canSeekPrev} onPress={onSeekPrev}>
-            <FaAngleLeft />
-          </Button>
-          <Button isIconOnly disabled={!canSeekNext} onPress={onSeekNext}>
-            <FaAngleRight />
-          </Button>
+          <div className="flex gap-4">
+            <Button isIconOnly disabled={!canSeekPrev} onPress={onSeekPrev} size="lg">
+              <FaAnglesLeft />
+            </Button>
+            <Button isIconOnly disabled={!canSeekPrev} onPress={onSeekPrev} size="lg">
+              <FaAngleLeft />
+            </Button>
+          </div>
+          <div>
+
+          </div>
+          <div className="flex gap-4">
+            <Button isIconOnly disabled={!canSeekNext} onPress={onSeekNext} size="lg">
+              <FaAngleRight />
+            </Button>
+            <Button isIconOnly disabled={!canSeekPrev} onPress={onSeekPrev} size="lg">
+              <FaAnglesRight />
+            </Button>
+          </div>
         </div>
       </CardBody>
     </Card>
