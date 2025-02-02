4d7398df54dc20bdbb8f02ad64a76d0145ce5f25
rca <nem@nem.x0.to>
2025-02-02 15:03:10 +0900

    impl some wip

--- src/pages/presenter/index.tsx
+++ src/pages/presenter/index.tsx
@@ -1,9 +1,19 @@
 import { PresenterBlockDirectSeekSlide } from "@/components/presenter/blocks/directSeekSlide";
 import { PresenterBlockPageScript } from "@/components/presenter/blocks/pageScript";
 import { PresenterBlockSessionStatus } from "@/components/presenter/blocks/presentationStatus";
+import { usePresenterContext } from "@/contexts/presenter-context";
+import { usePresenterOperation } from "@/hooks/use-presenter-operation";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 
-export default function PresenterIndexPage() {
+export default async function PresenterIndexPage() {
+
+  const presenterContext = usePresenterContext();
+
+  const wsSender = presenterContext.sendWsMessage;
+  const sessionInfo = await presenterContext.sessionInfo;
+  const sessionState = presenterContext.state
+
+  const presenterOperation = usePresenterOperation(wsSender, sessionInfo, sessionState);
 
   return (
     <>
@@ -14,15 +24,21 @@ export default function PresenterIndexPage() {
           className="container"
         >
           <PresenterBlockDirectSeekSlide
-            canSeekPrev={false}
-            canSeekNext={true}
-            onSeekPrev={() => { }}
-            onSeekNext={() => { }}
+            canSeekNextPage={presenterOperation.canChangeToNextPage()}
+            canSeekPrevPage={presenterOperation.canChangeToPrevPage()}
+            canSeekNextStep={presenterOperation.canChangeToNextStep()}
+            canSeekPrevStep={presenterOperation.canChangeToPrevStep()}
+            onSeekNextPage={presenterOperation.changeToNextPage}
+            onSeekPrevPage={presenterOperation.changeToPrevPage}
+            onSeekNextStep={presenterOperation.seekToNextStep}
+            onSeekPrevStep={presenterOperation.seekToPrevStep}
           ></PresenterBlockDirectSeekSlide>
           <PresenterBlockSessionStatus
             slideTitle="スライドタイトル"
             totalPageNumber={10}
             currentPageIndexNumber={1}
+            currentPageTotalStepNumber={4}
+            currentPageCurrentStepNumber={2}
             currentPageTitle="あああ"
           ></PresenterBlockSessionStatus>
           <PresenterBlockPageScript
