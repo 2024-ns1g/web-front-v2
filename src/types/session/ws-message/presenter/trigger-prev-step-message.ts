ee41416b3a90b961c2906f12e3e2c879a1597246
rca <nem@nem.x0.to>
2025-02-04 15:18:15 +0900

    add some property

--- src/types/session/ws-message/presenter/trigger-prev-step-message.ts
+++ src/types/session/ws-message/presenter/trigger-prev-step-message.ts
@@ -1,9 +1,11 @@
-import { z } from "zod";
+import { custom, z } from "zod";
 
 // =左矢印キーに近い挙動
 export const presenterWsTriggerPrevStepMessageSchema = z.object({
   requestType: z.literal('TRIGGER_PREV_STEP'),
   data: z.object({
+    currentPageIndex: z.number(),
+    currentStepIndex: z.number(),
   })
 });
 
