c6ee336ef0700c3678183d2959f68980c4c7cb33
rca <nem@nem.x0.to>
2025-02-06 04:49:34 +0900

    define!

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  define: {
+    'import.meta.env.VITE_API_HOST': JSON.stringify(process.env.VITE_API_HOST || ''),
+    'import.meta.env.VITE_API_DEFAULT_TIMEOUT': JSON.stringify(process.env.VITE_API_DEFAULT_TIMEOUT || ''),
+    'import.meta.env.VITE_LOG_LEVEL': JSON.stringify(process.env.VITE_LOG_LEVEL || ''),
+    'import.meta.env.VITE_MIN_LOG_LEVEL': JSON.stringify(process.env.VITE_MIN_LOG_LEVEL || ''),
+  }
 })
